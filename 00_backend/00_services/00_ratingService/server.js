898fb698b6cfdcbec0803e2fcfda66ad0d6088b3
samis0707 <github@samis-ellsaesser.de>
2023-05-06 00:17:00 +0200

    datenbank eingebunden MongoDB Atlas

--- 00_backend/00_services/00_ratingService/server.js
+++ 00_backend/00_services/00_ratingService/server.js
@@ -5,3 +5,34 @@
     dotenv
     cors
 */
+
+//Express.JS
+const express = require('express');
+const app = express();
+
+//DOTENV einbinden
+require('dotenv').config();
+
+//Mongoose einbinden für Datenbankabfragen
+const mongoose = require('mongoose');
+
+//DOTENV Konfigurationen beziehen
+const port = process.env.PORT;
+
+//Cross-Site Requests ermöglichen
+const cors = require('cors');
+
+//Datenbankverbindung aufbauen
+mongoose.connect(
+    process.env.MONGODB_URI, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+
+    },
+);
+
+const db = mongoose.connection;
+db.on('error', (error) => console.error(error))
+db.once('open', () => console.log("Database Connected"));
+
+//Routes
---
+++ 02_documentation/readme.md
@@ -0,0 +1,4 @@
+# Dokumentation des Projekts
+
+- Wahl eines geeigneten Hosting Anbieters
+- Zusammenfassung der Teildokumentationen im Backend
