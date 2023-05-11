c3fe7397d0df0f8e50785e4e3fa6ce3b9d8fe74b
samis0707 <github@samis-ellsaesser.de>
2023-05-11 09:51:25 +0200

    Probleme mit Country und id methode

--- 00_backend/00_services/00_ratingService/ratingRouter.js
+++ 00_backend/00_services/00_ratingService/ratingRouter.js
@@ -8,8 +8,19 @@ const router = express.Router();
 router.use(express.json());
 
 // checkRating Middleware
-
-
+async function checkRating(req, res, next) {
+    let rating;
+    try {
+        rating = await dbSchema.js.findById(req.params.id);
+        if (rating = null) {
+            return res.status(404).json({ message: 'Rating nicht verfügbar'});
+        }
+    } catch (err) {
+          return res.status(500).json({ message: err.message });
+    }
+    res.rating = rating;
+    next();
+}
 
 
 // Alle Ratings ausgeben lassen
@@ -24,7 +35,7 @@ router.get('/all', async(req, res) => {
 })
 
 // Rating nach DB-ID
-router.get('/:id', checkRating, (req, res) => {
+router.get('/id/:id', checkRating, (req, res) => {
     res.json(res.rating);
 })
 
@@ -37,6 +48,7 @@ router.get('/country/:country', async(req, res) => {
         res.json(countryRatings);
     } catch(err) {
         res.json({ message: err.message });
+        console.log("Keine Ratings verfügbar")
     }
 })
 module.exports = router;
